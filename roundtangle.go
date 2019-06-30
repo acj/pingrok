d6a0e40cc4571550e18e4ebece6b5d2f64446af3
Adam Jensen <acjensen@gmail.com>
2019-06-30 16:36:36 -0400

    Add circular buffer implementation

---
+++ roundtangle.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+)
+
+type Buffer struct {
+	buf []Reply
+	currentOffset int
+	saturated bool
+}
+
+func NewCircularBuffer(size int) *Buffer {
+	return &Buffer{
+		buf: make([]Reply, size, size),
+	}
+}
+
+func (b *Buffer) Snapshot() []Reply {
+	snap := make([]Reply, len(b.buf))
+
+	if b.saturated {
+		n := copy(snap[0:len(b.buf)-b.currentOffset], b.buf[len(b.buf)-(len(b.buf)-b.currentOffset):])
+		if n != (len(b.buf) - b.currentOffset) {
+			log.Fatalf("unexpected short copy: %d bytes", n)
+		}
+		n = copy(snap[n:], b.buf[0:b.currentOffset])
+		if n != b.currentOffset {
+			log.Fatalf("unexpected short copy: %d bytes", n)
+		}
+	} else {
+		n := copy(snap[:], b.buf[:])
+		if n != len(b.buf) {
+			log.Fatalf("unexpected short copy: %d bytes", n)
+		}
+	}
+	return snap
+}
+
+func (b *Buffer) Insert(value Reply) {
+	b.buf[b.currentOffset] = value
+	//log.Printf("offset is now (%d + 1) %% %d = %d", b.currentOffset, len(b.buf), (b.currentOffset + 1) % len(b.buf))
+	b.currentOffset = (b.currentOffset + 1) % len(b.buf)
+
+	if b.currentOffset == 0 {
+		b.saturated = true
+	}
+}
+
+func (b *Buffer) Size() int {
+	return len(b.buf)
+}
---
+++ roundtangle_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"log"
+	"testing"
+)
+
+func TestNewCircularBuffer_returnsUsableBuffer(t *testing.T) {
+	b := NewCircularBuffer(10)
+
+	if b == nil {
+		t.Error("expected a buffer but got nil")
+	}
+}
+
+func TestSnapshot_returnsCorrectlySizedBuffer(t *testing.T) {
+	expected := 10
+	b := NewCircularBuffer(expected)
+
+	snap := b.Snapshot()
+	if len(snap) != expected {
+		t.Errorf("expected buffer of size %d, but it was %d", expected, len(snap))
+	}
+}
+
+func TestSnapshot_returnsCorrectSnapshotForSaturatedBuffer(t *testing.T) {
+	b := NewCircularBuffer(4)
+
+	b.Insert(Reply{Latency: 1.0})
+	b.Insert(Reply{Latency: 2.0})
+	b.Insert(Reply{Latency: 3.0})
+	b.Insert(Reply{Latency: 4.0})
+	b.Insert(Reply{Latency: 5.0})
+
+	snap := b.Snapshot()
+
+	log.Printf("Buf: %v", b.buf)
+	log.Printf("Snap: %v", snap)
+	expected := Reply{Latency: 2.0}
+	if snap[0] != expected {
+		t.Errorf("expected '%v' but got '%v'", expected, snap[0])
+	}
+	expected = Reply{Latency: 3.0}
+	if snap[1] != expected {
+		t.Errorf("expected '%v' but got '%v'", expected, snap[0])
+	}
+	expected = Reply{Latency: 4.0}
+	if snap[2] != expected {
+		t.Errorf("expected '%v' but got '%v'", expected, snap[0])
+	}
+	expected = Reply{Latency: 5.0}
+	if snap[3] != expected {
+		t.Errorf("expected '%v' but got '%v'", expected, snap[0])
+	}
+}
+
+func TestInsert_addsValueToBuffer(t *testing.T) {
+	b := NewCircularBuffer(10)
+	expected := Reply{Latency: 1.0}
+
+	b.Insert(expected)
+
+	if snap := b.Snapshot(); snap[0] != expected {
+		t.Errorf("expected '%v' but got '%v'", expected, snap[0])
+	}
+}
+
+func TestInsert_overwritesOldestValueWhenBufferIsFull(t *testing.T) {
+	b := NewCircularBuffer(1)
+
+	b.Insert(Reply{Latency: 1.0})
+	b.Insert(Reply{Latency: 2.0})
+
+	snap := b.Snapshot()
+	expected := Reply{Latency: 2.0}
+	if snap[0] != expected {
+		t.Errorf("expected '%v' but got '%v'", expected, snap[0])
+	}
+}
