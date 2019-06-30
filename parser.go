a9b2f5381dc49c6dfedb9e77203ebb9faf77991d
Adam Jensen <acjensen@gmail.com>
2019-06-30 16:36:45 -0400

    Add parser for ping output

---
+++ parser.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Parser struct {
+	Next chan Reply
+}
+
+func NewParser(r io.Reader) *Parser {
+	p := &Parser{
+		Next: make(chan Reply, 100),
+	}
+
+	go func () {
+		defer close(p.Next)
+
+		scanner := bufio.NewScanner(r)
+		for scanner.Scan() {
+			tokens := strings.Split(scanner.Text(), " ")
+			offset, err := strconv.ParseFloat(tokens[0], 64)
+			if err != nil {
+				fmt.Printf("offset parse error for '%s': %v", tokens[0], err)
+			}
+			latency, err := strconv.ParseFloat(tokens[1], 64)
+			if err != nil {
+				fmt.Printf("latency parse error for '%s': %v", tokens[1], err)
+			}
+
+			p.Next<- Reply{offset, latency}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+	}()
+
+	return p
+}
