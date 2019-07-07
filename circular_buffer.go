7329814cb145d8d634f92fbda8bad6de888e92c4
Adam Jensen <acjensen@gmail.com>
2019-07-07 17:10:58 -0400

    Add mutex to synchronize access to shared circular buffer

--- circular_buffer.go
+++ circular_buffer.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"sync"
 )
 
 type CircularBuffer struct {
+	mux sync.Mutex
 	buf []LatencyReport
 	count int
 	currentOffset int
@@ -19,6 +21,7 @@ func NewCircularBuffer(size int) *CircularBuffer {
 func (b *CircularBuffer) Snapshot() []LatencyReport {
 	snap := make([]LatencyReport, len(b.buf))
 
+	b.mux.Lock()
 	n := copy(snap[0:b.count-b.currentOffset], b.buf[b.count-(b.count-b.currentOffset):])
 	if n != (b.count - b.currentOffset) {
 		log.Fatalf("unexpected short copy: %d bytes", n)
@@ -27,18 +30,23 @@ func (b *CircularBuffer) Snapshot() []LatencyReport {
 	if n != b.currentOffset {
 		log.Fatalf("unexpected short copy: %d bytes", n)
 	}
+	b.mux.Unlock()
 	return snap
 }
 
 func (b *CircularBuffer) Insert(value LatencyReport) {
+	b.mux.Lock()
 	b.buf[b.currentOffset] = value
 
 	if b.count < len(b.buf) {
 		b.count++
 	}
 	b.currentOffset = (b.currentOffset + 1) % len(b.buf)
+	b.mux.Unlock()
 }
 
 func (b *CircularBuffer) Size() int {
+	b.mux.Lock()
+	defer b.mux.Unlock()
 	return len(b.buf)
 }
