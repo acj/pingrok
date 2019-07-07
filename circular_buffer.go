d3db59d951b1f1df22057f799c51e2f5cbcfce4d
Adam Jensen <acjensen@gmail.com>
2019-07-06 21:01:12 -0400

    Simplify circular buffer snapshot implementation

--- circular_buffer.go
+++ circular_buffer.go
@@ -6,8 +6,8 @@ import (
 
 type CircularBuffer struct {
 	buf []LatencyReport
+	count int
 	currentOffset int
-	saturated bool
 }
 
 func NewCircularBuffer(size int) *CircularBuffer {
@@ -19,31 +19,24 @@ func NewCircularBuffer(size int) *CircularBuffer {
 func (b *CircularBuffer) Snapshot() []LatencyReport {
 	snap := make([]LatencyReport, len(b.buf))
 
-	if b.saturated {
-		n := copy(snap[0:len(b.buf)-b.currentOffset], b.buf[len(b.buf)-(len(b.buf)-b.currentOffset):])
-		if n != (len(b.buf) - b.currentOffset) {
-			log.Fatalf("unexpected short copy: %d bytes", n)
-		}
-		n = copy(snap[n:], b.buf[0:b.currentOffset])
-		if n != b.currentOffset {
-			log.Fatalf("unexpected short copy: %d bytes", n)
-		}
-	} else {
-		n := copy(snap[:], b.buf[:])
-		if n != len(b.buf) {
-			log.Fatalf("unexpected short copy: %d bytes", n)
-		}
+	n := copy(snap[0:b.count-b.currentOffset], b.buf[b.count-(b.count-b.currentOffset):])
+	if n != (b.count - b.currentOffset) {
+		log.Fatalf("unexpected short copy: %d bytes", n)
+	}
+	n = copy(snap[n:], b.buf[0:b.currentOffset])
+	if n != b.currentOffset {
+		log.Fatalf("unexpected short copy: %d bytes", n)
 	}
 	return snap
 }
 
 func (b *CircularBuffer) Insert(value LatencyReport) {
 	b.buf[b.currentOffset] = value
-	b.currentOffset = (b.currentOffset + 1) % len(b.buf)
 
-	if b.currentOffset == 0 {
-		b.saturated = true
+	if b.count < len(b.buf) {
+		b.count++
 	}
+	b.currentOffset = (b.currentOffset + 1) % len(b.buf)
 }
 
 func (b *CircularBuffer) Size() int {
