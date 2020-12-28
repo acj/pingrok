77cd901ec0aa2e0a3eb79aa2b18c8f09bd75c4d0
Adam Jensen <acjensen@gmail.com>
2020-12-27 20:47:12 -0500

    Switch to tables to support a selected state

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/rivo/tview"
 	"math"
 	"os"
-	"strconv"
 	"time"
 )
 
@@ -16,65 +15,78 @@ import (
 // * Create an interface that decouples the display engine from ping
 // * Create a generator that oscillates smoothly between 0 and some known latency -- good for end-to-end testing
 
+const placeholderSelectCellText = "Select a cell to view latency and time"
+
 func main() {
 	var timeWindowSeconds = flag.Int("t", 30, "seconds of data to display")
 	var samplesPerSecond = flag.Int("r", 10, "number of pings per second")
 	var targetHost = flag.String("h", "192.168.1.1", "the host to ping")
 	flag.Parse()
 
-	yAxisLabels := tview.NewGrid().
-		SetSize(*samplesPerSecond, 1, 0, 0).
-		SetBorders(false)
+	var currentSnapshot []LatencyDataPoint
 
-	xAxisLabels := tview.NewGrid().
-		SetSize(1, *timeWindowSeconds, 0, 0).
-		SetBorders(false)
+	yAxisLabels := tview.NewTable()
+	xAxisLabels := tview.NewTable()
 
-	heatmap := tview.NewGrid().
-		SetSize(*samplesPerSecond, *timeWindowSeconds, 0, 0).
-		SetBorders(false)
+	heatmap := tview.NewTable().
+		SetBorders(false).
+		SetSelectable(true, true)
+
+	infoCenter := tview.NewTable()
+	infoCenterLeftCell := tview.NewTableCell(placeholderSelectCellText).SetExpansion(1)
+	infoCenterRightCell := tview.NewTableCell("")
+	infoCenter.SetCell(0, 0, infoCenterLeftCell).
+		SetCell(0, 1, infoCenterRightCell)
 
 	rootGrid := tview.NewGrid().
-		SetRows(0, 1).
+		SetRows(0, 1, 1).
 		SetColumns(10, 0).
 		SetBorders(true).
 		AddItem(yAxisLabels, 0, 0, 1, 1, 0, 0, false).
 		AddItem(xAxisLabels, 1, 1, 1, 1, 0, 0, false).
-		AddItem(heatmap, 0, 1, 1, 1, 0, 0, false)
+		AddItem(heatmap, 0, 1, 1, 1, 0, 0, true).
+		AddItem(infoCenter, 2, 1, 1, 1, 0, 0, false)
 
 	app := tview.NewApplication().
 		SetRoot(rootGrid, true).
 		EnableMouse(true)
 
-	gridItems := make([][]*tview.Box, *samplesPerSecond)
-	for row := 0; row < *samplesPerSecond; row++ {
-		gridItems[row] = make([]*tview.Box, *timeWindowSeconds)
-	}
+	heatmap.SetSelectionChangedFunc(func(row, col int) {
+		if row > *samplesPerSecond || col > *timeWindowSeconds {
+			infoCenterLeftCell.SetText(placeholderSelectCellText)
+			return
+		}
+
+		dataPoint := currentSnapshot[row + col * *samplesPerSecond]
+		infoCenterLeftCell.SetText(fmt.Sprintf("Latency: %.02f ms @ Time Offset: %.02f seconds", dataPoint.Latency, dataPoint.TimeOffset))
+	})
 
 	for row := 0; row < *samplesPerSecond; row++ {
 		offsetMs := int(1000.0 * float64(row) / float64(*samplesPerSecond))
-		labelBox := tview.NewTextView().
-			SetTextAlign(tview.AlignLeft).
-			SetText(fmt.Sprintf("%d ms", offsetMs)).
+		cell := tview.NewTableCell(fmt.Sprintf("%d ms", offsetMs)).
+			SetAlign(tview.AlignRight).
 			SetTextColor(tcell.ColorWhite)
-		yAxisLabels.AddItem(labelBox, row, 0, 1, 1, 0, 0, false)
+		yAxisLabels.SetCell(row, 0, cell)
 	}
 
 	for col := 0; col < *timeWindowSeconds; col++ {
-		labelBox := tview.NewTextView().
-			SetTextAlign(tview.AlignLeft).
-			SetText(strconv.Itoa(col)).
-			SetTextColor(tcell.ColorWhite)
-		xAxisLabels.AddItem(labelBox, 0, col, 1, 1, 0, 0, false)
+		cell := tview.NewTableCell(fmt.Sprintf("%02d", col)).
+			SetAlign(tview.AlignCenter).
+			SetTextColor(tcell.ColorWhite).
+			SetExpansion(1)
+		xAxisLabels.SetCell(0, col, cell)
 	}
 
 	for row := 0; row < *samplesPerSecond; row++ {
 		for col := 0; col < *timeWindowSeconds; col++ {
-			emptyCell := tview.NewBox().SetBackgroundColor(tcell.ColorGreen)
-			heatmap.AddItem(emptyCell, row, col, 1, 1, 1, 1, false)
-			gridItems[row][col] = emptyCell
+			cell := tview.NewTableCell("").SetExpansion(1)
+			heatmap.SetCell(row, col, cell)
 		}
 	}
+
+	// Placeholder cell for nothing-is-selected state
+	heatmap.SetCell(*samplesPerSecond + 1, 0, tview.NewTableCell(""))
+	heatmap.Select(*samplesPerSecond + 1, 0)
 
 	// TODO: Handle mouse events
 	// TODO: Handle keyboard events
@@ -89,11 +101,11 @@ func main() {
 
 	go func() {
 		for {
-			snapshot := pinger.latencyReportCircularBuffer.Snapshot()
+			currentSnapshot = pinger.latencyReportCircularBuffer.Snapshot()
 
 			minLatency := math.MaxFloat64
 			maxLatency := 0.0
-			for _, dataPoint := range snapshot {
+			for _, dataPoint := range currentSnapshot {
 				if dataPoint.Latency >= maxLatency {
 					maxLatency = dataPoint.Latency
 				}
@@ -103,7 +115,10 @@ func main() {
 			}
 
 			app.QueueUpdateDraw(func() {
-				for idx, dataPoint := range snapshot {
+				haveFullSnapshot := false
+				selectedRow, selectedCol := heatmap.GetSelection()
+
+				for idx, dataPoint := range currentSnapshot {
 					row := idx % *samplesPerSecond
 					col := idx / *samplesPerSecond
 
@@ -113,9 +128,22 @@ func main() {
 
 					latencyRange := maxLatency - minLatency
 					scaledRedLevel := int32(((dataPoint.Latency - minLatency) / latencyRange) * 255.0)
-					color := tcell.NewRGBColor(int32(latencyRange) - scaledRedLevel, 0, 0)
-					gridItems[row][col].SetBackgroundColor(color)
+					color := tcell.NewRGBColor(scaledRedLevel, 0, 0)
+					heatmap.GetCell(row, col).SetBackgroundColor(color)
+
+					haveFullSnapshot = row == *samplesPerSecond - 1 && col == *timeWindowSeconds - 1
 				}
+
+				// Track the currently selected cell, if any
+				if haveFullSnapshot && heatmap.HasFocus() {
+					if selectedCol > 0 {
+						heatmap.Select(selectedRow, selectedCol - 1)
+					} else {
+						heatmap.Select(selectedRow, selectedCol)
+					}
+				}
+
+				infoCenterRightCell.SetText(fmt.Sprintf("Min: %.02f ms / Max: %.02f ms", minLatency, maxLatency))
 			})
 
 			time.Sleep(1 * time.Second)
